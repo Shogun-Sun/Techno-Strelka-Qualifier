27ee945d2fb6eff20dc17d01ae92f0fec1357ca0
makar-md <150604154+makar-md@users.noreply.github.com>
2025-02-05 09:43:40 +0300

    add login front

---
+++ public/CSS/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  darkMode: 'selector',
+  theme: {
+    extend: {
+      colors: {
+        'text-clr': 'rgb(27, 31, 40)',
+      },
+      borderRadius: {
+        '10': '10px', // Добавляем кастомное значение
+      },
+      boxShadow:{
+        'my': '-2px 3px 10px rgb(180,180,180)', // x, y, рассеивание
+      }
+    },
+  },
+  plugins: [],
+}
